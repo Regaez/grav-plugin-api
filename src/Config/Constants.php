e293fef3b679c7930b3091f725d3ba3837696c6e
Thomas Threadgold <tj.threadgold@gmail.com>
2019-08-08 22:28:14 +0200

    feat: create constants file

---
+++ src/Config/Constants.php
@@ -0,0 +1,21 @@
+<?php
+namespace GravApi\Config;
+
+/**
+ * Class Constants
+ * @package GravApi\Config
+ */
+class Constants
+{
+    // Resource types
+    const TYPE_PAGE = 'page';
+    const TYPE_USER = 'user';
+    const TYPE_PLUGIN = 'plugin';
+    const TYPE_CONFIG = 'config';
+
+    // Endpoints
+    const ENDPOINT_PAGE = '/pages';
+    const ENDPOINT_USER = '/users';
+    const ENDPOINT_PLUGIN = '/plugins';
+    const ENDPOINT_CONFIG = '/configs';
+}
