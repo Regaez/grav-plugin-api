8d321138e989ce7808bbc9045ca82b7522836737
Thomas Threadgold <tj.threadgold@gmail.com>
2019-08-18 15:02:55 +0200

    refactor: create new Constants

--- src/Config/Constants.php
+++ src/Config/Constants.php
@@ -7,6 +7,8 @@ namespace GravApi\Config;
  */
 class Constants
 {
+    const DEFAULT_ROUTE = 'api';
+
     // Resource types
     const TYPE_PAGE = 'page';
     const TYPE_USER = 'user';
@@ -18,4 +20,20 @@ class Constants
     const ENDPOINT_USER = '/users';
     const ENDPOINT_PLUGIN = '/plugins';
     const ENDPOINT_CONFIG = '/configs';
+
+    // Methods
+    const METHOD_GET = 'get';
+    const METHOD_PATCH = 'patch';
+    const METHOD_POST = 'post';
+    const METHOD_DELETE = 'delete';
+
+    /**
+     * All available methods
+     */
+    const METHODS = [
+        METHOD_GET,
+        METHOD_PATCH,
+        METHOD_POST,
+        METHOD_DELETE
+    ];
 }
