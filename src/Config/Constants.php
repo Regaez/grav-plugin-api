1387324af368e08b93767a9dbe95a909b08efd4e
Thomas Threadgold <tj.threadgold@gmail.com>
2019-08-19 23:13:16 +0200

    refactor: add new constants

--- src/Config/Constants.php
+++ src/Config/Constants.php
@@ -21,19 +21,25 @@ class Constants
     const ENDPOINT_PLUGIN = '/plugins';
     const ENDPOINT_CONFIG = '/configs';
 
+    // All available endpoints
+    const ENDPOINTS = [
+        Constants::ENDPOINT_PAGE,
+        Constants::ENDPOINT_USER,
+        Constants::ENDPOINT_PLUGIN,
+        Constants::ENDPOINT_CONFIG
+    ];
+
     // Methods
     const METHOD_GET = 'get';
     const METHOD_PATCH = 'patch';
     const METHOD_POST = 'post';
     const METHOD_DELETE = 'delete';
 
-    /**
-     * All available methods
-     */
+    // All available methods
     const METHODS = [
-        METHOD_GET,
-        METHOD_PATCH,
-        METHOD_POST,
-        METHOD_DELETE
+        Constants::METHOD_GET,
+        Constants::METHOD_PATCH,
+        Constants::METHOD_POST,
+        Constants::METHOD_DELETE
     ];
 }
