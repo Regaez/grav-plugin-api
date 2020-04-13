20365954c4242c83f3869b6e28fe3196e7b8407c
Thomas Threadgold <tj.threadgold@gmail.com>
2020-04-13 19:09:04 +0200

    tests: cover new PagesHandler functionality

--- tests/unit/Handlers/PagesHandlerTest.php
+++ tests/unit/Handlers/PagesHandlerTest.php
@@ -23,18 +23,19 @@ final class PagesHandlerTest extends Test
     /** @var PagesHandler $handler */
     protected $handler;
 
-    protected function _before($attributeFields = array())
+    protected function _before($attributes = array(), $endpoints = array())
     {
         $grav = Fixtures::get('grav');
         $this->grav = $grav();
 
-        Config::instance([
+        // initialise our default test config
+        $config = [
             'endpoints' => [
                 Constants::ENDPOINT_PAGE => [
                     Constants::METHOD_GET => [
                         'enabled' => true,
                         'auth' => false,
-                        'fields' => $attributeFields
+                        'fields' => $attributes
                     ],
                     Constants::METHOD_PATCH => [
                         'enabled' => true,
@@ -50,7 +51,14 @@ final class PagesHandlerTest extends Test
                     ]
                 ]
             ]
-        ]);
+        ];
+
+        // override our default config if custom endpoints are given
+        if (count($endpoints) > 0) {
+            $config['endpoints'] = $endpoints;
+        }
+
+        Config::instance($config);
 
         $this->handler = new PagesHandler();
         $this->response = new Response();
@@ -70,6 +78,99 @@ final class PagesHandlerTest extends Test
         $this->assertEquals(200, $response->getStatusCode());
     }
 
+    public function testGetPagesAuthShouldReturnStatus200(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_GET => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'GET',
+                'REQUEST_URI' => '/api/pages'
+            ])
+        );
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('development');
+        $user->authenticated = true;
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->getPages($request, $this->response, [
+            'page' => 'test'
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testGetPagesAdvancedAccessShouldReturnLimitedPages(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_GET => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'GET',
+                'REQUEST_URI' => '/api/pages',
+            ])
+        );
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('greg');
+        $user->authenticated = true;
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->getPages($request, $this->response, [
+            'page' => 'test'
+        ]);
+
+        $data = json_decode($response->getBody()->__toString());
+        $this->assertEquals(2, $data->meta->count);
+    }
+
+    public function testGetPagesAdvancedAccessShouldReturnEmptyArray(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_GET => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'GET',
+                'REQUEST_URI' => '/api/pages',
+            ])
+        );
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('tom');
+        $user->authenticated = true;
+        $user->set('api.advanced_access.pages.get', []);
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->getPages($request, $this->response, [
+            'page' => 'home'
+        ]);
+
+        $data = json_decode($response->getBody()->__toString());
+        $this->assertEquals(0, $data->meta->count);
+    }
+
     public function testGetPageShouldReturnStatus200(): void
     {
         $request = Request::createFromEnvironment(
@@ -84,6 +185,96 @@ final class PagesHandlerTest extends Test
         ]);
 
         $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testGetPageAuthShouldReturnStatus200(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_GET => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'GET',
+                'REQUEST_URI' => '/api/pages/test'
+            ])
+        );
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('development');
+        $user->authenticated = true;
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->getPage($request, $this->response, [
+            'page' => 'test'
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testGetPageAdvancedAccessShouldReturnStatus200(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_GET => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'GET',
+                'REQUEST_URI' => '/api/pages/test',
+            ])
+        );
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('tom');
+        $user->authenticated = true;
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->getPage($request, $this->response, [
+            'page' => 'test'
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testGetPageAdvancedAccessShouldReturnStatus401(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_GET => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'GET',
+                'REQUEST_URI' => '/api/pages/home',
+            ])
+        );
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('tom');
+        $user->authenticated = true;
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->getPage($request, $this->response, [
+            'page' => 'home'
+        ]);
+
+        $this->assertEquals(401, $response->getStatusCode());
     }
 
     public function testGetPageShouldReturnStatus404(): void
@@ -260,6 +451,107 @@ final class PagesHandlerTest extends Test
         $this->assertEquals('/test-order', $data->attributes->route);
     }
 
+    public function testNewPageAuthShouldReturnStatus200(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_POST => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'POST',
+                'REQUEST_URI' => '/api/pages'
+            ])
+        )->withParsedBody([
+            'route' => '/test-auth',
+            'header' => [
+                'title' => 'Test page'
+            ],
+            'order' => 1
+        ]);
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('development');
+        $user->authenticated = true;
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->newPage($request, $this->response, []);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testNewPageAdvancedAccessShouldReturnStatus200(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_POST => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'POST',
+                'REQUEST_URI' => '/api/pages',
+            ])
+        )->withParsedBody([
+            'route' => '/test/advanced-auth',
+            'header' => [
+                'title' => 'Test page'
+            ]
+        ]);
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('tom');
+        $user->authenticated = true;
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->newPage($request, $this->response, []);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testNewPageAdvancedAccessShouldReturnStatus401(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_POST => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'POST',
+                'REQUEST_URI' => '/api/pages',
+            ])
+        )->withParsedBody([
+            'route' => '/unauthorised-route',
+            'header' => [
+                'title' => 'Test page'
+            ],
+            'order' => 1
+        ]);
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('tom');
+        $user->authenticated = true;
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->newPage($request, $this->response, []);
+
+        $this->assertEquals(401, $response->getStatusCode());
+    }
+
     public function testUpdatePageShouldReturnStatus400IfNoRouteGiven(): void
     {
         $request = Request::createFromEnvironment(
@@ -400,6 +692,118 @@ final class PagesHandlerTest extends Test
         $this->assertEquals('/test-order', $data->attributes->route);
     }
 
+    public function testUpdatePageAuthShouldReturnStatus200(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_PATCH => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'PATCH',
+                'REQUEST_URI' => '/api/pages'
+            ])
+        )->withParsedBody([
+            'route' => '/test-page',
+            'header' => [
+                'title' => 'Test page 2',
+                'taxonomy' => [
+                    'category' => [
+                        'blog'
+                    ]
+                ]
+            ],
+            'content' => 'This is some new authored content'
+        ]);
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('development');
+        $user->authenticated = true;
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->getPage($request, $this->response, [
+            'page' => 'test-page'
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testUpdatePageAdvancedAccessShouldReturnStatus200(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_PATCH => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'PATCH',
+                'REQUEST_URI' => '/api/pages',
+            ])
+        )->withParsedBody([
+            'route' => '/test',
+            'header' => [
+                'title' => 'Test page'
+            ]
+        ]);
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('tom');
+        $user->authenticated = true;
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->getPage($request, $this->response, [
+            'page' => 'test'
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testUpdatePageAdvancedAccessShouldReturnStatus401(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_PATCH => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'PATCH',
+                'REQUEST_URI' => '/api/pages',
+            ])
+        )->withParsedBody([
+            'route' => '/home',
+            'header' => [
+                'title' => 'Change title',
+            ],
+            'content' => 'This is some failed content update'
+        ]);
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('tom');
+        $user->authenticated = true;
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->getPage($request, $this->response, [
+            'page' => 'home'
+        ]);
+
+        $this->assertEquals(401, $response->getStatusCode());
+    }
+
     public function testDeletePageShouldReturnStatus404(): void
     {
         $request = Request::createFromEnvironment(
@@ -444,5 +848,101 @@ final class PagesHandlerTest extends Test
 
         $this->assertEquals(204, $responseTest->getStatusCode());
         $this->assertEquals(204, $responseOrder->getStatusCode());
+    }
+
+    public function testDeletePageAuthShouldReturnStatus204(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_DELETE => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'DELETE',
+                'REQUEST_URI' => '/api/pages/test-auth'
+            ])
+        );
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('development');
+        $user->authenticated = true;
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->deletePage(
+            $request,
+            $this->response,
+            [ 'page' => 'test-auth' ]
+        );
+
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+
+    public function testDeletePageAdvancedAuthShouldReturnStatus204(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_DELETE => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'DELETE',
+                'REQUEST_URI' => '/api/pages/test/advanced-auth'
+            ])
+        );
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('tom');
+        $user->authenticated = true;
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->deletePage(
+            $request,
+            $this->response,
+            [ 'page' => 'test/advanced-auth' ]
+        );
+
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+
+    public function testDeletePageAdvancedAuthShouldReturnStatus401(): void
+    {
+        $this->_before([], [
+            Constants::ENDPOINT_PAGE => [
+                Constants::METHOD_DELETE => [
+                    'enabled' => true,
+                    'auth' => true
+                ]
+            ]
+        ]);
+
+        $request = Request::createFromEnvironment(
+            Environment::mock([
+                'REQUEST_METHOD' => 'DELETE',
+                'REQUEST_URI' => '/api/pages/home'
+            ])
+        );
+
+        // Decorate the request with the user (which AuthMiddleware would usually do)
+        $user = $this->grav['accounts']->load('tom');
+        $user->authenticated = true;
+        $request = $request->withAttribute('user', $user);
+
+        $response = $this->handler->deletePage(
+            $request,
+            $this->response,
+            [ 'page' => 'home' ]
+        );
+
+        $this->assertEquals(401, $response->getStatusCode());
     }
 }
